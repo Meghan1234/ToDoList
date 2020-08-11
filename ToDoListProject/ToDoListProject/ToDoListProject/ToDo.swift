bbae2126bcc4478e3fa379c3722a30663b1a8731
Meghan1234 <51449030+Meghan1234@users.noreply.github.com>
2020-08-10 18:01:26 -0700

    Add files via upload

---
+++ ToDoListProject/ToDoListProject/ToDoListProject/ToDo.swift
@@ -0,0 +1,18 @@
+//
+//  ToDo.swift
+//  ToDoListProject
+//
+//  Created by Apple on 8/10/20.
+//  Copyright © 2020 Meghan Jachna. All rights reserved.
+//
+
+import UIKit
+
+
+class ToDoClass {
+
+   var description = ""
+   var important = false
+
+}
+
